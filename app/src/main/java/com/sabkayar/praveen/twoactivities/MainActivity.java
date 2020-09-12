4c2405d79d2019d57464aad05d5d45995691bd3e
Praveen <pkchmits@gmail.com>
2020-09-12 15:40:57 +0530

    Passing data between two activities Back button or defining parent activity for easy navigation

--- app/src/main/java/com/sabkayar/praveen/twoactivities/MainActivity.java
+++ app/src/main/java/com/sabkayar/praveen/twoactivities/MainActivity.java
@@ -1,22 +1,58 @@
 package com.sabkayar.praveen.twoactivities;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.snackbar.Snackbar;
+import com.sabkayar.praveen.twoactivities.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = MainActivity.class.getSimpleName();
+    private ActivityMainBinding mBinding;
+
+    private static final String EXTRA_REPLY =
+            MainActivity.class.getName() + ".extra.REPLY";
+
+    private static final int RC_MESSAGE = 1;
+
+    public static Intent getIntent(String reply) {
+        Intent intent = new Intent();
+        intent.putExtra(EXTRA_REPLY, reply);
+        return intent;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        mBinding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(mBinding.getRoot());
     }
 
     public void launchSecondActivity(View view) {
         Log.d(TAG, "Button clicked!");
+        String message = mBinding.editTextMain.getText().toString();
+        if (!TextUtils.isEmpty(message)) {
+            startActivityForResult(SecondActivity.getIntent(this, message), RC_MESSAGE);
+        } else {
+            Snackbar.make(mBinding.buttonMain, "Enter some message to proceed!", Snackbar.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == RC_MESSAGE && resultCode == RESULT_OK) {
+            if (data != null && data.hasExtra(EXTRA_REPLY)) {
+                mBinding.textViewMessage.setText(data.getStringExtra(EXTRA_REPLY));
+                mBinding.textViewHeader.setVisibility(View.VISIBLE);
+            }
+        }
     }
 }
--- app/src/main/java/com/sabkayar/praveen/twoactivities/SecondActivity.java
+++ app/src/main/java/com/sabkayar/praveen/twoactivities/SecondActivity.java
@@ -1,14 +1,49 @@
 package com.sabkayar.praveen.twoactivities;
 
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.google.android.material.snackbar.Snackbar;
+import com.sabkayar.praveen.twoactivities.databinding.ActivitySecondBinding;
 
 public class SecondActivity extends AppCompatActivity {
+    private static final String EXTRA_MESSAGE = SecondActivity.class.getName() + "extra.MESSAGE";
+
+    public static Intent getIntent(Context context, String message) {
+        Intent intent = new Intent(context, SecondActivity.class);
+        intent.putExtra(EXTRA_MESSAGE, message);
+        return intent;
+    }
+
+    private ActivitySecondBinding mBinding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_second);
+        mBinding = ActivitySecondBinding.inflate(getLayoutInflater());
+        setContentView(mBinding.getRoot());
+
+        Intent intent = getIntent();
+        if (intent != null && intent.hasExtra(EXTRA_MESSAGE)) {
+            mBinding.textViewMessage.setText(intent.getStringExtra(EXTRA_MESSAGE));
+            mBinding.textHeader.setVisibility(View.VISIBLE);
+        }
+
+
+    }
+
+    public void returnReply(View view) {
+        String reply = mBinding.editTextReply.getText().toString();
+        if (!TextUtils.isEmpty(reply)) {
+            setResult(RESULT_OK, MainActivity.getIntent(reply));
+            finish();
+        } else {
+            Snackbar.make(mBinding.buttonReply, "Enter some reply to proceed!", Snackbar.LENGTH_SHORT).show();
+        }
     }
 }
